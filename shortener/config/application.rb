9565a010c8f43223d492d99045ab4f0198d9e925
4knahs <4knahs@gmail.com>
2015-11-17 14:58:35 +0100

    changes to shortener

--- shortener/config/application.rb
+++ shortener/config/application.rb
@@ -22,5 +22,6 @@ module Shortener
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.web_console.whiny_requests = false
   end
 end
