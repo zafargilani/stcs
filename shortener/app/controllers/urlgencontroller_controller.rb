04f138e41a409534025c83a3793c1bddf6cf7fe6
Zafar Gilani <zafar.gilani@cl.cam.ac.uk>
2015-11-21 00:30:09 +0000

    Time.now urlgencontroller

--- shortener/app/controllers/urlgencontroller_controller.rb
+++ shortener/app/controllers/urlgencontroller_controller.rb
@@ -16,7 +16,7 @@ class UrlgencontrollerController < ApplicationController
   def show
 
       begin
-        #log tweet ids and url token for each copied tweet
+        #log url click timestamp, tweet ids and url token for each copied tweet
         open('/home/cloud-user/clicks/clicks.txt', 'a') { |f|
           f.puts "#{Time.now}, #{params[:id]}, #{request.remote_ip}, #{cookies[:revisit]}, #{request.env["HTTP_USER_AGENT"]}"
         }
