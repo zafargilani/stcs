c4976377ae1f244b8ba64f6215d1e7274dd9bc5f
aknahs <4knahs@gmail.com>
2016-02-05 15:33:29 +0100

    trying to fix graph

--- shortener/config/routes.rb
+++ shortener/config/routes.rb
@@ -16,9 +16,9 @@ Rails.application.routes.draw do
   get '/:id' => "urlgencontroller#show"
   get '/gen/:id' => "urlgencontroller#generate" #of the form /gen/i?u=#{url}
   get '/r/:id' => "urlgencontroller#redirect"
-  get '/getjson/:id' => "urlgencontroller#jsongraph"
+  get '/graph/getjson/:id' => "urlgencontroller#jsongraph"
   #get '/g/getvector/:id' => "urlgencontroller#vectorgraph"
-  get '/graph' => "urlgencontroller#getgraph"
+  get '/graph/:id' => "urlgencontroller#getgraph"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
