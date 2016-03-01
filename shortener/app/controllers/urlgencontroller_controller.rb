d7b65ecfca621b7a2377dba2b547e4b394b3a06b
aknahs <4knahs@gmail.com>
2016-03-01 16:46:19 +0100

    attempt to cache clicks

--- shortener/app/controllers/urlgencontroller_controller.rb
+++ shortener/app/controllers/urlgencontroller_controller.rb
@@ -1,9 +1,29 @@
 require 'user_agent_parser'
 require 'shortener'
 require 'time'
-require '../models/clicksCache'
 
 class UrlgencontrollerController < ApplicationController
+
+  class ClicksCache
+
+    #def initialize(max)
+    @@cache = []
+    @@MAX = 500
+    #end
+
+    def self.insert val
+      @@cache << val
+      if @@cache.size > @@MAX
+        @@cache.shift #remove in FIFO order
+      end
+    end
+
+    def self.get
+      @@cache
+    end
+
+  end
+
 
   def generate
 
