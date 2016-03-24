1c7006d779850560e51df269d486131369436f0e
Cloud User <zafargilani@gmail.com>
2016-03-24 19:01:16 +0000

    BotOrNot changes

--- botornot-python/botornot.py
+++ botornot-python/botornot.py
@@ -4,23 +4,26 @@ import json
 import configparser
 import sys
 
-config = configparser.ConfigParser()
-config.sections()
+try:
+	config = configparser.ConfigParser()
+	config.sections()
 
-config.read('/home/cloud-user/stcs/config.ini')
+	config.read('/home/cloud-user/stcs/config.ini')
 
-twitter_app_auth = {
-        'consumer_key': config['DEFAULT']['bon_consumer_key'],
-        'consumer_secret': config['DEFAULT']['bon_consumer_secret'],
-        'access_token': config['DEFAULT']['bon_oauth_token'],
-        'access_token_secret': config['DEFAULT']['bon_oauth_secret']
-        }
+	twitter_app_auth = {
+        	'consumer_key': config['DEFAULT']['bon_consumer_key'],
+        	'consumer_secret': config['DEFAULT']['bon_consumer_secret'],
+        	'access_token': config['DEFAULT']['bon_oauth_token'],
+        	'access_token_secret': config['DEFAULT']['bon_oauth_secret']
+        	}
 
-#print(twitter_app_auth['consumer_key'])
+	#print(twitter_app_auth['consumer_key'])
 
-bon = botornot.BotOrNot(**twitter_app_auth)
+	bon = botornot.BotOrNot(**twitter_app_auth)
 
-#json_response = json.dumps(bon.check_account('@clayadavis'), sort_keys=True, indent=4, separators=(',', ': '))
-print(json.dumps(bon.check_account(sys.argv[1]), sort_keys=True, indent=4, separators=(',', ': ')))
-#print(json.dumps(bon.check_account('@clayadavis'), sort_keys=True, indent=4, separators=(',', ': ')))
+	#json_response = json.dumps(bon.check_account('@clayadavis'), sort_keys=True, indent=4, separators=(',', ': '))
+	print(json.dumps(bon.check_account(sys.argv[1]), sort_keys=True, indent=4, separators=(',', ': ')))
+	#print(json.dumps(bon.check_account('@clayadavis'), sort_keys=True, indent=4, separators=(',', ': ')))
+except:
+	pass
 
--- lib/classifier.rb
+++ lib/classifier.rb
@@ -2,28 +2,38 @@ require 'zlib'
 require 'json'
 #dates: Sun 2015-10-18 to Sat 2015-10-24
 
-out = "{"
+parsed_line = ""
+response = ""
+parsed_response = ""
+out = ""
 begin
   infile = open('/data/zf-twitter-data/2015-10-18.uk.txt.gz')
-  gz = Zlib::GzipReader.new(infile)
-  gz.each_line do |line|
-    parsed_line = JSON.parse(line)
-    #puts "@#{parsed_line["user"]["screen_name"]}"
-    #puts parsed_line["user"]["screen_name"]
-    response = %x(/usr/bin/python /home/cloud-user/stcs/botornot-python/botornot.py @#{parsed_line["user"]["screen_name"]})
-    parsed_response = JSON.parse(response)
-    if parsed_response["score"].to_f >= 0.5 #bot = 1, notbot/human = 0
-      #out = out + "\"@#{parsed_line['user']['screen_name']\"" + " : 1,"
-      puts "bot"
-    else
-      #out = out + "\"@#{parsed_line['user']['screen_name']\"" + " : 0,"
-      puts "notbot"
+  gzi = Zlib::GzipReader.new(infile)
+  gzi.each_line do |line|
+    begin
+      parsed_line = JSON.parse(line)
+      response = %x(/usr/bin/python /home/cloud-user/stcs/botornot-python/botornot.py @#{parsed_line["user"]["screen_name"]})
+      parsed_response = JSON.parse(response)
+      if parsed_response["score"].to_f >= 0.5 #bot = 1, notbot/human = 0
+        #output: tweet_id, user, botornot, followers, friends, retweets
+        out = out + "#{parsed_line['id']}, @#{parsed_line['user']['screen_name']}, 1, #{parsed_line['user']['followers_count']}, "
+        out = out + "#{parsed_line['user']['friends_count']}, #{parsed_line['retweet_count']}\n"
+      else
+        out = out + "#{parsed_line['id']}, @#{parsed_line['user']['screen_name']}, 0, #{parsed_line['user']['followers_count']}, "
+        out = out + "#{parsed_line['user']['friends_count']}, #{parsed_line['retweet_count']}\n"
+      end
+      #puts out
+      open('/data2/zf-twitter-data/2015-10-18.uk.out.txt', 'a') { |outfile|
+        outfile.puts out
+      }
+      out = "" #reset out
+    rescue
+      next
     end
   end
 rescue => e
   puts e
 end
-out = out + "}"
-#puts out
-#gz.close
+#gzi.close
+#gzo.close
 
