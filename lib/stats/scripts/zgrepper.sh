6e108c8f75fe14f9b1b56e4276217bd740f156ff
Syed Gilani <szuhg2@cl.cam.ac.uk>
2016-05-27 17:56:28 +0100

    minor changes

--- lib/stats/scripts/zgrepper.sh
+++ lib/stats/scripts/zgrepper.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-zgrep "\"screen_name\":\"SCREENNAME\"" /local/scratch/twitter-data/2016-4-1.uk.txt.gz >> 2016-4.bot.SCREENNAME
-zgrep "\"screen_name\":\"SCREENNAME\"" /local/scratch/twitter-data/2016-4-1.uk.txt.gz >> 2016-4.human.SCREENNAME
+zgrep "\"screen_name\":\"SCREENNAME\"" /data/reserve/twitter/2016-4-1.uk.txt.gz >> SCREENNAME
 
