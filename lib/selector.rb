967a6512b714f356d3801bf1027464e84d227d34
Syed Gilani <szuhg2@cl.cam.ac.uk>
2016-06-14 19:31:50 +0100

    generalising selector

--- lib/selector.rb
+++ lib/selector.rb
@@ -1,8 +1,11 @@
-# usage: ruby selector.rb /full/path/input/file.txt.gz
+# usage: ruby selector.rb /full/path/input/file.txt.gz LOWER_LIMIT UPPER_LIMIT > selected.txt
 require 'zlib'
 require 'json'
 # This script writes out most popular Twitter accounts from data collected via Streaming API
 # Criteria is either large number of followers/friends or large number of favourite tweets
+
+LOWER_LIMIT = ARGV[1].to_i
+UPPER_LIMIT = ARGV[2].to_i
 
 parsed_line = ""
 out = ""
@@ -12,26 +15,31 @@ begin
   gzi.each_line do |line|
     begin
       parsed_line = JSON.parse(line)
-      # output: screen_name, followers_count, friends_count, favourites_count, statuses_count
-      # perspective 1: user following and friends
-      if parsed_line["user"]["followers_count"].to_i >= 50000000 or parsed_line["user"]["friends_count"] >= 50000000
+      if parsed_line["user"]["followers_count"].to_i >= LOWER_LIMIT and parsed_line["user"]["followers_count"] <= UPPER_LIMIT
         out = out + "@#{parsed_line['user']['screen_name']}, #{parsed_line['user']['followers_count']}, #{parsed_line['user']['friends_count']}, "
-        out = out + "#{parsed_line["user"]["favourites_count"]}, #{parsed_line["user"]["statuses_count"]}\n"
-        open('/local/scratch/szuhg2/classifier_data/2016-4-1.fnf', 'a') { |outfile| # followers and friends
-          outfile.puts out
-        }
-      # perspective 2: favourties = 25% or more of tweets
-      elsif parsed_line["user"]["followers_count"].to_i >= 50000000 and parsed_line["user"]["favourites_count"].to_i >= (0.25*parsed_line["user"]["statuses_count"].to_f).to_i
-        out = out + "@#{parsed_line['user']['screen_name']}, #{parsed_line['user']['followers_count']}, #{parsed_line['user']['friends_count']}, "
-        out = out + "#{parsed_line["user"]["favourites_count"]}, #{parsed_line["user"]["statuses_count"]}\n"
-        open('/local/scratch/szuhg2/classifier_data/2016-4-1.fav', 'a') { |outfile| # favourites = 25% of all tweets
-          outfile.puts out
-        }
+        out = out + "#{parsed_line['user']['favourites_count']}, #{parsed_line['user']['statuses_count']}\n"
+	puts out
       end
-      #puts out
-      #open('/data2/zf-twitter-data/2016-4-1.selected', 'a') { |outfile|
-      #  outfile.puts out
-      #}
+      ## output: screen_name, followers_count, friends_count, favourites_count, statuses_count
+      ## perspective 1: user following and friends
+      #if parsed_line["user"]["followers_count"].to_i >= 50000000 or parsed_line["user"]["friends_count"] >= 50000000
+      #  out = out + "@#{parsed_line['user']['screen_name']}, #{parsed_line['user']['followers_count']}, #{parsed_line['user']['friends_count']}, "
+      #  out = out + "#{parsed_line["user"]["favourites_count"]}, #{parsed_line["user"]["statuses_count"]}\n"
+      #  open('/local/scratch/szuhg2/classifier_data/2016-4-1.fnf', 'a') { |outfile| # followers and friends
+      #    outfile.puts out
+      #  }
+      ## perspective 2: favourties = 25% or more of tweets
+      #elsif parsed_line["user"]["followers_count"].to_i >= 50000000 and parsed_line["user"]["favourites_count"].to_i >= (0.25*parsed_line["user"]["statuses_count"].to_f).to_i
+      #  out = out + "@#{parsed_line['user']['screen_name']}, #{parsed_line['user']['followers_count']}, #{parsed_line['user']['friends_count']}, "
+      #  out = out + "#{parsed_line["user"]["favourites_count"]}, #{parsed_line["user"]["statuses_count"]}\n"
+      #  open('/local/scratch/szuhg2/classifier_data/2016-4-1.fav', 'a') { |outfile| # favourites = 25% of all tweets
+      #    outfile.puts out
+      #  }
+      #end
+      ##puts out
+      ##open('/data2/zf-twitter-data/2016-4-1.selected', 'a') { |outfile|
+      ##  outfile.puts out
+      ##}
       out = "" #reset out
     rescue
       next
