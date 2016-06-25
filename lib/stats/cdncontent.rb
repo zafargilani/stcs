4de3ec3df95b543f6b2e5847a624861281ff334c
Syed Gilani <szuhg2@cl.cam.ac.uk>
2016-06-25 01:07:50 +0100

    cdncontent - wget script

---
+++ lib/stats/cdncontent.rb
@@ -0,0 +1,59 @@
+# usage: ruby cdncontent.rb /fully/qualified/path/to/directory[accts] > cdncontent.txt
+require 'zlib'
+require 'json'
+
+# read accounts from a file
+# read accts/files from a directory
+acct_list = Dir.entries(ARGV[0])
+acct_list.delete(".") # remove . from the list
+acct_list.delete("..") # remove .. from the list
+acct_list.sort!
+
+# get size of user content uploaded on Twitter CDN (*.twimg.*) from raw tweets/json
+# dump this in an output file from time to time
+
+pline = ""
+out = ""
+#content_size_list = []
+content_size = 0
+
+acct_list.each do |acct|
+  begin
+    infile = open("#{ARGV[0]}/#{acct}")
+    #gzi = Zlib::GzipReader.new(infile)
+    #gzi.each_line do |line|
+    infile.each_line do |line|
+      begin
+        pline = JSON.parse(line)
+	if pline["user"]["screen_name"] == acct
+	  if pline["entities"]["media"]["media_url"].include? "twimg" # /[a-z]*.twimg.[a-z]*/
+	    content_size += `wget #{pline["entities"]["media"]["media_url"]} --spider --server-response -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}'`
+	  end
+	elsif pline["retweeted_status"]["user"]["screen_name"] == acct
+	  if pline["retweeted_status"]["entities"]["media"]["media_url"].include? "twimg" # /[a-z]*.twimg.[a-z]*/
+	    content_size += `wget #{pline["retweeted_status"]["entities"]["media"]["media_url"]} --spider --server-response -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}'`
+	  end
+	elsif pline["quoted_status"]["user"]["screen_name"] == acct
+	  if pline["quoted_status"]["entities"]["media"]["media_url"].include? "twimg" # /[a-z]*.twimg.[a-z]*/
+	    content_size += `wget #{pline["quoted_status"]["entities"]["media"]["media_url"]} --spider --server-response -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}'`
+	  end
+	end
+      rescue
+        next
+      end
+    end
+    # if you don't like JSON
+    out = "#{acct}: #{content_size} bytes" # bytes to KB to MB
+    puts out
+    #out_json = {
+    #  "screen_name" => "#{acct}",
+    #  "total_content_size" => "#{content_size}"
+    #}
+    #puts out_json
+    # reset vars
+    content_size = 0
+  rescue => e
+    puts e
+  end
+end
+
