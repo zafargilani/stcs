65c74ec686abb733f5ef92a08e4a1c05efc98634
Syed Gilani <szuhg2@cl.cam.ac.uk>
2016-09-09 17:59:54 +0100

    botornot changes

--- lib/botornot.rb
+++ lib/botornot.rb
@@ -1,4 +1,4 @@
-# usage: ruby botornot.rb /full/path/input/list.txt > /full/path/output/file.txt
+# usage: ruby botornot.rb /full/path/input/list.txt /full/path/output/file.txt
 require 'zlib'
 require 'json'
 
@@ -13,13 +13,20 @@ acct_list.each do |acct|
   begin
     response = %x(/usr/bin/python /local/scratch/szuhg2/stcs/botornot-python/botornot.py #{acct})
     parsed_response = JSON.parse(response)
-    if parsed_response["score"].to_f >= 0.45 #bot = 1, notbot/human = 0
-      puts "#{acct}, bot"
+    if parsed_response["score"].to_f >= 0.45 # observation shows >45% of OSN traffic comes from bots
+      out = "#{acct}, bot"
+    elsif parsed_response["score"].to_f < 0.45
+      out = "#{acct}, human"
+    elsif parsed_response.include? "Sorry, that page does not exist"
+      out = "#{acct}, NA"
     else
-      puts "#{acct}, human"
+      out = "#{acct}, NA"
     end
-  rescue
-    next
+  rescue => e
+    puts e
   end
+  File.open("#{ARGV[1]}", 'a') do |f|
+    f.puts(out)
+  end # auto file close
 end
 
